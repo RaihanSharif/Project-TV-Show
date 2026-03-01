f3366ce3f9e353df0e8707128863fdcc452de1e5
Raihan Sharif <raihansharif92@gmail.com>
2026-03-01 12:52:28 +0000

    seperate fetch for shows and episodes for a given show ID

--- fetchTVData.js
+++ fetchTVData.js
@@ -1,7 +1,7 @@
 // function to fetch data from API.
 
-async function fetchTVData() {
-    const url = "https://api.tvmaze.com/shows/82/episodes";
+export async function fetchEpisodes(showId) {
+    const url = `https://api.tvmaze.com/shows/${82}/episodes`;
 
     try {
         const response = await fetch(url);
@@ -11,8 +11,21 @@ async function fetchTVData() {
 
         return await response.json();
     } catch (error) {
-        alert(`There was an error fetching the data ${error}`);
+        alert(`There was an error fetching episodes ${error}`);
     }
 }
 
-export default fetchTVData;
+export async function fetchShows() {
+    const url = `https://api.tvmaze.com/shows`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        alert(`There was an error fetching shows ${error}`);
+    }
+}
