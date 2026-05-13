f76f97dc675c108e75a831f7994b1e7128ecd738
Tomislav Dukez <tomdu3@ymail.com>
2026-05-13 22:27:40 +0100

    add change fetchTVData w/fetchShows and cachedFetch funcs

--- fetchEpisodes.js
+++
@@ -1,9 +0,0 @@
-export async function fetchAllEpisodes(showId) {
-    const url = `https://api.tvmaze.com/shows/${showId}/episodes`;
-
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`Could not fetch episodes: ${response.status}`);
-    }
-    return response.json();
-}
